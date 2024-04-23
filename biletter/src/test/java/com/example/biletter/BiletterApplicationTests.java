ea3bbf0e6aa2cd93af6c453ed98a68f665fc037f
angeclara <141249648+angeclara@users.noreply.github.com>
2024-04-23 13:26:16 +0200

    made biletter package

---
+++ biletter/src/test/java/com/example/biletter/BiletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiletterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
